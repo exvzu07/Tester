05d61120809a31c04b18fc5a30689422a8182e96
exvzu07 <rafidesu07@gmail.com>
2024-06-11 16:50:03 +0700

    Test dlu

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-saldo_awal = 5000
+saldo_awal = 10000
 print('Saldo awal Anda adalah : ' + str(saldo_awal))
 deposit = input('Masukkan saldo yang ingin di input : ')
 saldo_total = int(saldo_awal) + int(deposit)
 print('Saldo total Anda adalah : ' + str(saldo_total))
 # User bisa melunasi hutang jika uangnya cukup
 # User harus melakukan deposit ulang jika saldonya kurang
-hutang = 40_000
+hutang = 75_000
 
 while saldo_total < hutang:
     print('Hutang Masih Belum Lunas, Lakukan Deposit Ulang Hingga Lunas')
